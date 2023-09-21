b9a20f528fc96c0ba7fc12288a5b4b9084e60e94
“malcolmmutare” <mutarem@gmail.com>
2023-09-21 17:14:36 +0200

    changed background

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,10 +5,10 @@ import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
 const Main = () => {
   return (
   <div id='main'>
-    <div className='w-full h-screen absolute top-0 left-0 bg-blue-500/40'>
+    <div className='w-full h-screen absolute top-0 left-0 bg-blue-800/40'>
     <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
         <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Malcolm Mutare</h1>
-        <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a
+        <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
         <TypeAnimation
            sequence={[
         // Same substring at the start will only be typed out once, initially
