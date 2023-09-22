486f3e6f19fd552c0d8eb9d0b1197426ba5fe415
“malcolmmutare” <mutarem@gmail.com>
2023-09-22 07:29:03 +0200

    added work component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,21 +5,21 @@ import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
 const Main = () => {
   return (
   <div id='main'>
-    <div className='w-full h-screen absolute top-0 left-0 bg-blue-800/40'>
+    <div className='w-full h-screen absolute top-0 left-0 bg-blue/50'>
     <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
         <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Malcolm Mutare</h1>
         <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
         <TypeAnimation
            sequence={[
         // Same substring at the start will only be typed out once, initially
-        'Full Stack Developer',
-        2000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Tech Enthusiast',
-        2000,
-        'Full Stack Developer',
-        1000,
-        'Tech Enthusiast',
-        2000
+        '<Full Stack Developer </>',
+        3000, // wait 1s before replacing "Mice" with "Hamsters"
+        '<With an Object Oriented Approach </>',
+        3000,
+        '<Full Stack Developer </>',
+        3000,
+        '<With an Object Oriented Approach </>',
+        3000
       ]}
       wrapper="div"
       speed={50}
