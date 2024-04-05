9fdd7a5d27b0a3cd27b6bd4827008cb3e3f45908
“malcolmmutare” <mutarem@gmail.com>
2024-04-05 18:46:40 +0200

    feat added footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,23 @@
+import Socials from "./Socials"
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials containerStyles="flex gap-x-6 xl:mx-0 mb-4"
+            iconStyles="text-primary dark:text-white/70 text-[20px] hover:text-white
+            dark:hover:text-primary transition-all"
+          />
+          {/* copyright */}
+
+          <div className="text-muted-foreground">
+            Copyright &copy; Malcolm Mutare. All Rights Reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
