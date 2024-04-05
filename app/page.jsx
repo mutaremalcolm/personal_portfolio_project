6da4a9e2f9a51902f9d077472162885358f30e89
“malcolmmutare” <mutarem@gmail.com>
2024-04-05 17:30:19 +0200

    feat:reviews/other projects section, added other projects section

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
-import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+import Reviews from "@/components/Reviews";
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
     <Hero />
     <Services />
     <Work />
+    <Reviews />
    </main>
   );
 }
