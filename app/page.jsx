e7f8e65b0ae203d89078c6df01b63206ee300e49
“malcolmmutare” <mutarem@gmail.com>
2024-04-05 18:05:45 +0200

    feat: added call to action

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 import Reviews from "@/components/Reviews";
+import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
     <Services />
     <Work />
     <Reviews />
+    <Cta />
    </main>
   );
 }
