3ff4a47e1af3880fd178c6075782c0b23bbc3b58
“malcolmmutare” <mutarem@gmail.com>
2024-05-16 09:23:22 +0200

    chore: code clean-up and resume update

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,5 @@
 import Hero from "@/components/Hero";
-import Services from "@/components/Services";
 import Work from "@/components/Work";
-import Reviews from "@/components/Reviews";
-import Cta from "@/components/Cta";
 import Projects from "./projects/page";
 import Contact from "./contact/page";
 
@@ -10,11 +7,7 @@ export default function Home() {
   return (
    <main>
     <Hero />
-    {/* <Services /> */}
-    {/* <Work /> */}
     <Projects />
-    {/* <Reviews /> */}
-    {/* <Cta /> */}
     <Contact />
    </main>
   );
